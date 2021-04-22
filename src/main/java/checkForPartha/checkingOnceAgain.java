8acef7de3a8402c2c7316fc33f011a9da87518ab
SKDK <muthukumaran.shanmugam@directlinegroup.co.uk>
2021-04-22 14:51:28 +0100

    first commit

---
+++ src/main/java/checkForPartha/checkingOnceAgain.java
@@ -0,0 +1,7 @@
+package checkForPartha;
+
+public class checkingOnceAgain {
+    public static void displaySomething(String param){
+        System.out.println("This is another package for Partha with :" + param);
+    }
+}
