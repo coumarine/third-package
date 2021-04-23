b1e90ffe8257b7f408b2950f77e458bf03e9d530
SMK <coumarine@gmail.com>
2021-04-23 10:40:28 +0100

    Check commit email

--- src/main/java/checkForPartha/checkingOnceAgain.java
+++ src/main/java/checkForPartha/checkingOnceAgain.java
@@ -3,5 +3,6 @@ package checkForPartha;
 public class checkingOnceAgain {
     public static void displaySomething(String param){
         System.out.println("This is another package for Partha with :" + param);
+
     }
 }
